e0287876973c58bed71f7eca5c08c2db2317589c
sialorama <sialorama@gmail.com>
2024-08-29 10:33:34 +0200

    First upload

---
+++ src/test/java/fr/multisurfbackend/MultisurfbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.multisurfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultisurfbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
